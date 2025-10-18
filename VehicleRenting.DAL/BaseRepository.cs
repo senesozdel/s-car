89ad172736483292b77c94ad8bc4529044f8fa3d
Salih Enes ÖZDEL <salihenesozdel@gmail.com>
2025-10-18 23:56:44 +0300

    comment is deleted

--- VehicleRenting.DAL/BaseRepository.cs
+++ VehicleRenting.DAL/BaseRepository.cs
@@ -9,10 +9,8 @@ namespace VehicleRenting.DAL
 
         protected BaseRepository(IConfiguration configuration)
         {
-            // 1️⃣ Önce environment variable'dan oku (Render gibi ortamlarda)
             var envConnection = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 
-            // 2️⃣ Eğer env yoksa (örneğin localde) appsettings.json'dan al
             _connectionString = !string.IsNullOrEmpty(envConnection)
                 ? envConnection
                 : configuration.GetConnectionString("DefaultConnection");
